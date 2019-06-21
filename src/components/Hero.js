2d2d9027792798c2b7eca839d719401870ddfd57
Rahul Syal <syal2512@gmail.com>
2019-06-21 16:57:05 +0530

    added team section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,32 +1,35 @@
-import React from 'react';
-import styled from 'styled-components';
-import Image from '../assets/hero6.jpg';
-import Header from './Header';
+import React from "react";
+import styled from "styled-components";
+import Image from "../assets/hero6.jpg";
+import Header from "./Header";
 
 const Background = styled.section`
-    background-image: url(${Image});
-    background-size: cover;
-    background-attachment: fixed;
+  background-image: url(${Image});
+  background-size: cover;
+  background-attachment: fixed;
 `;
-
-export default class Hero extends React.Component{
-    render(){
-        return(
-            <Background>
-                <Header />
-                <section className="hero is-fullheight">
-                <div className="hero-body">
-                    <div className="container">
-                    <h1 className="title has-text-white is-size-1">
-                        PyData Jalandhar
-                    </h1>
-                    <h2 className="subtitle has-text-white">
-                        Become a part of the largest open source community.
-                    </h2>
-                    </div>
-                </div>
-                </section>
-            </Background>
-        )
-    }
+const HeadingContainer = styled.div`
+  margin-left: 3.5rem;
+`;
+const Title = styled.p`
+  font-size: 50px;
+`;
+export default class Hero extends React.Component {
+  render() {
+    return (
+      <Background>
+        <Header />
+        <section className="hero is-fullheight">
+          <div className="hero-body">
+            <HeadingContainer className="container">
+              <Title className="title has-text-white">PyData Jalandhar</Title>
+              <h2 className="subtitle has-text-white">
+                Become a part of the largest open source community.
+              </h2>
+            </HeadingContainer>
+          </div>
+        </section>
+      </Background>
+    );
+  }
 }
