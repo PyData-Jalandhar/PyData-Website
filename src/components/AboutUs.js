1e994577b0c78144b160735311f4ce6ab67a7a5a
Anushka Beri <berianushka@gmail.com>
2019-06-24 06:40:16 +0530

    Minor changes in about sections' meetups image.

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -28,18 +28,6 @@ const AboutSections = styled.div`
   }
 `;
 
-// const Text = styled.h2`
-//     @media only screen and (max-width:768px) {
-//         text-align:center;
-//     }
-// `;
-
-// const Description = styled.h3`
-//     @media only screen and (max-width:768px) {
-//         text-align:center;
-//     }
-// `;
-
 const ImageOne = styled.div`
   background-image: url(${PictureOne});
   background-size: cover;
@@ -78,19 +66,13 @@ export default class AboutUs extends React.Component {
             tools to share ideas and learn from each other.
           </p>
         </AboutSections>
-        {/* <div className="container has-text-centered">
-          <p className="is-size-5 has-text-grey">
-            PyData is an educational program of NumFOCUS, a non-profit
-            organization in the United States. PyData provides a forum for the
-            international community of users and developers of data analysis
-            tools to share ideas and learn from each other.
-          </p>
-        </div> */}
 
         <Space className="columns" />
 
         <AboutSections className="columns is-vcentered">
-          <ImageOne className="column is-5" />
+          <div className="column is-5">
+            <ImageOne />
+          </div>
           <div className="column is-7 has-text-justified">
             <h2 className="is-size-3 has-text-weight-bold has-text-black">
               Open-Source Community
@@ -120,47 +102,10 @@ export default class AboutUs extends React.Component {
               trends and making them learna and work on new things.
             </h3>
           </div>
-          <ImageTwo className="column is-5" />
+          <div className="column is-5"> 
+            <ImageTwo className="is-pulled-right"/>
+          </div>
         </AboutSections>
-
-        {/* <div className="columns is-vcentered">
-          <div className="column is-3 is-offset-2" >
-              <ImageOne />
-          </div>
-          <div className="column is-5 has-text-justified">
-            <Text className="is-size-3 has-text-weight-bold has-text-black">
-              Open-Source Community
-            </Text>
-            <br />
-            <br />
-            <Description className="is-size-5 has-text-grey">
-              Our community is diverse and highly motivated. It helps to help
-              create and maintain communication within and among open source
-              projects motivate and efficiency organize groups of remote
-              individuals.
-            </Description>
-          </div>
-        </div>
-
-
-        <div className="columns is-vcentered">
-          <div className="column is-3 is-offset-2" >
-              <ImageTwo />
-          </div>
-          <div className="column is-5 has-text-justified">
-            <Text className="is-size-3 has-text-weight-bold has-text-black">
-              Meetups
-            </Text>
-            <br />
-            <br />
-            <Description className="is-size-5 has-text-grey">
-            Meetups are the best way to collaborate. They help the community
-              to grow not only in context to members but also contributing to
-              giving talks and making members aware of latest technological
-              trends and making them learna and work on new things.
-            </Description>
-          </div>
-        </div> */}
       </section>
     );
   }
